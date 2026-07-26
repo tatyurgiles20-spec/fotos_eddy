3e2a8c821b2abde36ff74ebe14726fb13ccfc557
tatyurgiles20-spec <tatyurgiles20@gmail.com>
2026-07-26 00:28:06 -0500

    cloud

---
+++ .open-next/cloudflare/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"NEXT_PUBLIC_SUPABASE_URL":"https://hsvlmjibfzgaajozlqqm.supabase.co","NEXT_PUBLIC_SUPABASE_ANON_KEY":"sb_publishable_7OEwX-HVbfbRw6tW3iSTUA_VXkPHj9y","ADMIN_EMAIL":"tatyurgiles20@gmail.com"};
+export const development = {"NEXT_PUBLIC_SUPABASE_URL":"https://hsvlmjibfzgaajozlqqm.supabase.co","NEXT_PUBLIC_SUPABASE_ANON_KEY":"sb_publishable_7OEwX-HVbfbRw6tW3iSTUA_VXkPHj9y","ADMIN_EMAIL":"tatyurgiles20@gmail.com"};
+export const test = {};
---
+++ .open-next/server-functions/default/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"NdGZPw7QlhfSZymLJstMmfO8FzAGe7+CRWVOdwObf3c=\"\n}"
